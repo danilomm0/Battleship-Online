13ff0f66d5cd080d29c395b0495cbb840245ae10
danilomm0 <danilomm@arizona.edu>
2024-12-10 19:46:44 -0700

    Update homepage.js

--- public/js/homepage.js
+++ public/js/homepage.js
@@ -23,12 +23,3 @@ function logout() {
 function dashboard() {
   window.location.href = "/account/" + username;
 }
-
-function generateLobbyCode() {
-  const characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
-  let code = "";
-  for (let i = 0; i < 6; i++) {
-    code += characters.charAt(Math.floor(Math.random() * characters.length));
-  }
-  return code;
-}
