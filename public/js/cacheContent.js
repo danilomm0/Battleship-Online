ec65fbd0ffcc7e77f75d861d2c691118d07ad344
danilomm0 <danilomm@arizona.edu>
2024-12-10 19:07:46 -0700

    Remembers user login status
    Changes what buttons are visible on the main page depending on whether or not the user is logged in.

--- public/js/cacheContent.js
+++ public/js/cacheContent.js
@@ -2,11 +2,11 @@
  * Store the difficulty which is grabbed from the current URL
  */
 function storeDifficulty() {
-    // url params
-    const urlParams = new URLSearchParams(window.location.search);
-    const difficulty = urlParams.get("difficulty");
-    // store in local cache
-    localStorage.setItem("gameDifficulty", difficulty);
+  // url params
+  const urlParams = new URLSearchParams(window.location.search);
+  const difficulty = urlParams.get("difficulty");
+  // store in local cache
+  localStorage.setItem("gameDifficulty", difficulty);
 }
 
 /**
@@ -16,8 +16,8 @@ function storeDifficulty() {
  * @param {Map} board the playerboard
  */
 function storeGameBoard(type, board) {
-    // store board locally
-    localStorage.setItem(type, JSON.stringify(Array.from(board)));
+  // store board locally
+  localStorage.setItem(type, JSON.stringify(Array.from(board)));
 }
 
 /**
@@ -26,14 +26,14 @@ function storeGameBoard(type, board) {
  * @returns Int -> the game difficulty
  */
 function retrieveDifficulty() {
-    const difficulty = localStorage.getItem("gameDifficulty");
-    if (difficulty) {
-        return difficulty;
-    }
-    console.error(
-        "Couldnt get the difficulty from local storage :( Returning max difficulty!"
-    );
-    return 4;
+  const difficulty = localStorage.getItem("gameDifficulty");
+  if (difficulty) {
+    return difficulty;
+  }
+  console.error(
+    "Couldnt get the difficulty from local storage :( Returning max difficulty!"
+  );
+  return 4;
 }
 
 /**
@@ -43,39 +43,37 @@ function retrieveDifficulty() {
  * @returns Map of the game board requested
  */
 function retrieveGameBoard(type) {
-    const storedBoard = localStorage.getItem(type);
-    if (storedBoard) {
-        return new Map(JSON.parse(storedBoard));
-    }
-    console.error("Couldnt get board from local storage. :(");
-    return null;
+  const storedBoard = localStorage.getItem(type);
+  if (storedBoard) {
+    return new Map(JSON.parse(storedBoard));
+  }
+  console.error("Couldnt get board from local storage. :(");
+  return null;
 }
 
 /**
- * 
+ *
  * @param {*} username The username setting of this user
  */
 function writeLoginStatus(username) {
-    localStorage.setItem("battleShipLogin", username)
+  localStorage.setItem("battleShipLogin", username);
 }
-
 
 /**
  * Clears the login status from local storage, signifying a logout
  */
 function clearLoginStatus() {
-    localStorage.removeItem("battleShipLogin");
+  localStorage.removeItem("battleShipLogin");
 }
 
-
 /**
- * 
+ *
  * @returns The username if logged in else null
  */
 function getLoginStatus() {
-    const username = localStorage.getItem("battleShipLogin");
-    if (username) {
-        return username;
-    }
-    return null;
+  const username = localStorage.getItem("battleShipLogin");
+  if (username) {
+    return username;
+  }
+  return null;
 }
---
+++ public/js/homepage.js
@@ -0,0 +1,37 @@
+let username = null;
+
+function load() {
+  console.log("this shit is hella gay");
+  username = getLoginStatus();
+  console.log(username);
+  if (username) {
+    console.log(username);
+    d3.select("#dashboard").classed("hidden", false);
+    d3.select("#logout").classed("hidden", false);
+    d3.select("#login").classed("hidden", true);
+  }
+}
+
+function login() {
+  window.location.href = "/login";
+}
+
+function logout() {
+  d3.select("#dashboard").classed("hidden", true);
+  d3.select("#logout").classed("hidden", true);
+  d3.select("#login").classed("hidden", false);
+  clearLoginStatus();
+}
+
+function dashboard() {
+  window.location.href = "/account/" + username;
+}
+
+function generateLobbyCode() {
+  const characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+  let code = "";
+  for (let i = 0; i < 6; i++) {
+    code += characters.charAt(Math.floor(Math.random() * characters.length));
+  }
+  return code;
+}
