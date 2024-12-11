5e03f78b3e655198ea7e1441daf032f0ccf6556a
danilomm0 <danilomm@arizona.edu>
2024-12-11 14:09:31 -0700

    try again

--- public/js/homepage.js
+++ public/js/homepage.js
@@ -2,69 +2,83 @@ let username = null;
 const channel = new BroadcastChannel("socketChannel");
 
 function load() {
-    username = getLoginStatus();
-    if (username) {
-        d3.select("#dashboard").classed("hidden", false);
-        d3.select("#logout").classed("hidden", false);
-        d3.select("#login").classed("hidden", true);
-    }
+  username = getLoginStatus();
+  if (username) {
+    d3.select("#dashboard").classed("hidden", false);
+    d3.select("#logout").classed("hidden", false);
+    d3.select("#login").classed("hidden", true);
+  }
 }
 
 function login() {
-    window.location.href = "/login";
+  window.location.href = "/login";
 }
 
 function logout() {
-    d3.select("#dashboard").classed("hidden", true);
-    d3.select("#logout").classed("hidden", true);
-    d3.select("#login").classed("hidden", false);
-    clearLoginStatus();
+  d3.select("#dashboard").classed("hidden", true);
+  d3.select("#logout").classed("hidden", true);
+  d3.select("#login").classed("hidden", false);
+  clearLoginStatus();
 }
 
 function dashboard() {
-    window.location.href = "/account/" + username;
+  window.location.href = "/account/" + username;
 }
 
 function hidePopup(temp) {
-    d3.select("#popup").classed("hidden", temp);
-    d3.selectAll(".button").classed("disable", !temp);
-    d3.selectAll(".button2").classed("disable", !temp);
-    d3.selectAll(".mode-card").classed("disable", !temp);
-    d3.selectAll(".nav-container").classed("disable", !temp);
+  d3.select("#popup").classed("hidden", temp);
+  d3.selectAll(".button").classed("disable", !temp);
+  d3.selectAll(".button2").classed("disable", !temp);
+  d3.selectAll(".mode-card").classed("disable", !temp);
+  d3.selectAll(".nav-container").classed("disable", !temp);
+  d3.select("#code").property("value", "");
+  d3.select("#error").classed("hidden", true);
 }
 
 function createGame() {
-    fetch('/api/createLobby', {
-        method: 'POST',
-    }).then((response) => {
-        if (response.ok) {
-            return response.json(); 
-        } else {
-            console.log("Failed to create lobby :(")
-        }
-    }).then((data) => {
-        console.log('The newGameID', data.gameID);
-        joinGameAPI(String(data.gameID))
-    }).catch((error) => {
-        console.error('Error:', error.message);
+  fetch("/api/createLobby", {
+    method: "POST",
+  })
+    .then((response) => {
+      if (response.ok) {
+        return response.json();
+      } else {
+        console.log("Failed to create lobby :(");
+      }
+    })
+    .then((data) => {
+      console.log("The newGameID", data.gameID);
+      joinGameAPI(String(data.gameID));
+    })
+    .catch((error) => {
+      console.error("Error:", error.message);
     });
 }
 
 function joinGame() {
-    let code = d3.select("#code").property("value");
-    joinGameAPI(code);
+  let code = d3.select("#code").property("value");
+  let output = joinGameAPI(code);
+  hideError(output);
+}
+function hideError(temp) {
+  d3.select("#error").classed("hidden", temp);
 }
 
 function joinGameAPI(gameID) {
-    fetch(`/api/gameStatus/${gameID}`, {
-        method: 'POST', // Use POST as specified in your route
-    }).then((response) => {
-        if (response.ok) {
-            socket.emit("joinRoom", String(code));
-        } else {
-            console.log('Game not found or some other error.');
-        }
-    }).catch((error) => {
-        console.error('Error making the request:', error);
+  fetch(`/api/gameStatus/${gameID}`, {
+    method: "POST", // Use POST as specified in your route
+  })
+    .then((response) => {
+      if (response.ok) {
+        socket.emit("joinRoom", String(code));
+        return true;
+      } else {
+        console.log("Game not found or some other error.");
+        return false;
+      }
+    })
+    .catch((error) => {
+      console.error("Error making the request:", error);
+      return false;
     });
 }
