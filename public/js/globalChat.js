621747b63a6d5cf5db2c042558f27b5f3151e494
Connor1719 <geoman.cr@gmail.com>
2024-12-12 16:20:17 -0700

    fix

--- public/js/globalChat.js
+++ public/js/globalChat.js
@@ -1,43 +1,45 @@
+username = getLoginStatus();
+
 function loadMessages() {
     window.globalSocket.emit("getChatHistory");
-  }
-  
-  function sendMessage() {
+}
+
+function sendMessage() {
     let message = d3.select("#message").property("value");
     const chat = d3.select(".messages-container");
     if (message) {
-      d3.select("#message").property("value", "");
-      const sender = username;
-      window.globalSocket.emit("sendGlobalMsg", { sender, message });
+        d3.select("#message").property("value", "");
+        const sender = username;
+        window.globalSocket.emit("sendGlobalMsg", { sender, message });
     }
-  }
-  
-  window.globalSocket.on("receiveGlobalMsg", (message) => {
+}
+
+window.globalSocket.on("receiveGlobalMsg", (message) => {
     console.log(`MESSAGE RECIEVED: ${message}`);
     const chat = d3.select(".messages-container");
     chat.append("div").attr("class", "message").html(`
         <div class="message-header">
           <span class="message-user">${message.sender}</span>
-          <span class="message-timestamp">${new Date(msg.timestamp).toLocaleTimeString([], { hour: 'numeric', minute: '2-digit' })
-      }</span>
+          <span class="message-timestamp">${new Date(message.timestamp).toLocaleTimeString([], { hour: 'numeric', minute: '2-digit' })
+        }</span>
         </div>
         <p class="message-text">${message.message}</p>`);
     const chatContainer = document.querySelector(".messages-container");
     chatContainer.scrollTop = chatContainer.scrollHeight;
-  });
-  
-  window.globalSocket.on("chatHistory", (messages) => {
+});
+
+window.globalSocket.on("chatHistory", (messages) => {
     console.log(messages);
     const chat = d3.select(".messages-container");
     messages.forEach((msg) => {
-      chat.append("div").attr("class", "message").html(`
+        chat.append("div").attr("class", "message").html(`
         <div class="message-header">
           <span class="message-user">${msg.sender}</span>
           <span class="message-timestamp">${new Date(msg.timestamp).toLocaleTimeString([], { hour: 'numeric', minute: '2-digit' })
-        }</span>
+            }</span>
         </div>
         <p class="message-text">${msg.message}</p>`);
     });
     const chatContainer = document.querySelector(".messages-container");
     chatContainer.scrollTop = chatContainer.scrollHeight;
-  });
+});
