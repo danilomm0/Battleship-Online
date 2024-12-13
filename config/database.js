8fe3c322de5dc222d65abe89651441e46e771583
Connor1719 <geoman.cr@gmail.com>
2024-12-13 12:10:05 -0700

    another cleanup

--- config/database.js
+++ config/database.js
@@ -1,13 +1,13 @@
 const mongoose = require('mongoose');
 
-// MongoDB connection setup
+// mongodb connection
 const connectDB = async () => {
   try {
     const conn = await mongoose.connect('mongodb://127.0.0.1:27017/battleship');
     console.log(`MongoDB Connected: ${conn.connection.host}`);
   } catch (err) {
     console.error(`Error: ${err.message}`);
-    process.exit(1); // Exit the process with failure
+    process.exit(1); // failure to connect just terminate project
   }
 };
 
